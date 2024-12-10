fc91a62cbd19fd3ec8835256c194e377073ad0db
noctuelles <paul@plouvel.dev>
2024-12-10 21:25:54 +0100

    Revert "feat: added ld r16 imm16. CPU reference bus."
    This reverts commit 05f03815ade1a7b00025ddbe07591fdc6d7b7936.

--- includes/CPU.hxx
+++ includes/CPU.hxx
@@ -1,30 +1,25 @@
 #ifndef CPU_H
 #define CPU_H
 
+#include <string>
 #include <array>
 #include <functional>
-#include <string>
 
-#include "Bus.hxx"
+#include "Memory.hxx"
 
-#define REG16_PAIR_GET(r1, r2) (static_cast<uint16_t>((r1 << 8) | r2))
-#define REG16_PAIR_SET(val, r1, r2) \
-    (r1 = static_cast<uint8_t>(val >> 8), r2 = static_cast<uint8_t>(val & 0xFF))
+#define REG16_PAIR_GET(r1,r2) (static_cast<uint16_t>((r1 << 8) | r2))
+#define REG16_PAIR_SET(val,r1,r2) (r1 = static_cast<uint8_t>(val >> 8), r2 = static_cast<uint8_t>(val & 0xFF))
 
 class CPU
 {
-  public:
-    using Reg = uint8_t CPU::*;
-    using RegPair = std::pair<uint8_t CPU::*, uint8_t CPU::*>;
-
+public:
     struct Instruction
     {
         std::string name;
-        size_t      cycles;
+        size_t cycles;
         void (CPU::*op)();
 
         static constexpr Instruction LD_R8_R8();
-        static constexpr Instruction LD_R16_IMM16();
         static constexpr Instruction LD_R8_IMM8();
         static constexpr Instruction AND_R8();
         static constexpr Instruction XOR_R8();
@@ -32,90 +27,78 @@ class CPU
         static constexpr Instruction ILL();
     };
 
-    template <typename RegType>
+    template<typename T>
     struct RegisterDescription
     {
         std::string name;
-        RegType     reg;
+        T CPU::* reg;
     };
 
     class BadRegisterException final : public std::exception
-    {
-    };
+    {};
 
     class IllegalInstructionException final : public std::exception
-    {
-    };
+    {};
 
     enum Flags
     {
-        ZERO       = 1 << 7,
-        SUBTRACT   = 1 << 6,
+        ZERO = 1 << 7,
+        SUBTRACT = 1 << 6,
         HALF_CARRY = 1 << 5,
-        CARRY      = 1 << 4
+        CARRY = 1 << 4
     };
 
-    explicit CPU(Bus& bus);
-    ~CPU();
+    CPU();
 
     void cycle();
 
     const std::string& get_str_inst() const;
 
-  private:
+private:
     void build_str_inst_r8(const std::string& reg);
     void build_str_inst_r8_xxx(const std::string& reg_dest, const std::string& val);
 
     void NOP();
     /**
-     * @brief Illegal instruction.
-     */
+    * @brief Illegal instruction.
+    */
     void ILL();
 
     /**
-     * @brief Load from 8-bit register to 8-bit register.
-     */
+    * @brief Load from 8-bit register to 8-bit register.
+    */
     void LD_R8_R8();
     /**
-     * @brief Load from 8-bit immediate to 8-bit register.
-     */
+    * @brief Load from 8-bit immediate to 8-bit register.
+    */
     void LD_R8_IMM8();
-    /**
-     * @brief Load from 16-bit immediate to 16-bit register.
-     */
-    void LD_R16_IMM16();
     void LD_R8_MEM_HL();
 
     /**
-     * @brief AND from 8-bit register to register A.
-     */
+    * @brief AND from 8-bit register to register A.
+    */
     void AND_R8();
     /**
-     * @brief AND from 8-bit register to register A.
-     */
+    * @brief AND from 8-bit register to register A.
+    */
     void OR_R8();
     /**
-     * @brief AND from 8-bit register to register A.
-     */
+    * @brief AND from 8-bit register to register A.
+    */
     void XOR_R8();
     /**
-     * @brief ADD from 8-bit register to register A.
-     */
+    * @brief ADD from 8-bit register to register A.
+    */
     void ADD_R8();
     /**
-     * @brief Increment by one a 8-bit register.
-     */
+    * @brief Increment by one a 8-bit register.
+    */
     void INC_R8();
 
-    /**
-     * @brief General Purpose Registers
-     */
-
-    uint8_t A, F;
-    uint8_t B, C;
-    uint8_t D, E;
-    uint8_t H, L;
-
+    uint8_t A,F;
+    uint8_t B,C;
+    uint8_t D,E;
+    uint8_t H,L;
     uint16_t PC;
     uint16_t SP;
 
@@ -128,28 +111,24 @@ class CPU
      */
     uint8_t opcode;
 
+    Memory memory;
     /**
-     * @brief Current instruction.
-     */
+    * @brief Current instruction.
+    */
     Instruction inst;
     /**
      * @brief String representation of the current instruction.
      */
     std::string str_inst;
+
     /**
-     * @brief Instruction lookup table.
-     */
+    * @brief Instruction lookup table.
+    */
     const static std::array<Instruction, 0x100> inst_lookup;
     /**
-     * @brief Lookup table for 8-bit register.
-     */
-    const static std::array<RegisterDescription<Reg>, 0x08> r8_lookup;
-    /**
-     * @brief Lookup table for 8-bit register.
-     */
-    const static std::array<RegisterDescription<RegPair>, 0x04> r16_lookup;
-
-    Bus& bus;
+    * @brief
+    */
+    const static std::array<RegisterDescription<uint8_t>, 8> r8_lookup;
 };
 
 #endif
