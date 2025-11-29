f1dcde3e5d9c2ad2126ae0ea2d0f2ef34ad3b5ba
noctuelles <plouvel@student.42.fr>
2025-11-29 15:55:40 +0100

    feat: added preference with keys.

---
+++ srcs/ui/Preference.hxx
@@ -0,0 +1,33 @@
+//
+// Created by plouvel on 11/29/25.
+//
+
+#ifndef GBEMU_PREFERENCE_HXX
+#define GBEMU_PREFERENCE_HXX
+
+#include <QDialog>
+
+QT_BEGIN_NAMESPACE
+namespace Ui
+{
+    class Preference;
+}
+QT_END_NAMESPACE
+
+class Preference final : public QDialog
+{
+    Q_OBJECT
+
+  public:
+    explicit Preference(QWidget* parent = nullptr);
+    ~Preference() override;
+
+    void accept() override;
+    void reject() override;
+
+  private:
+    QString         _bootRomPath;
+    Ui::Preference* _ui;
+};
+
+#endif  // GBEMU_PREFERENCE_HXX
